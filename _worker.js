ab2141769c81b5386c6c338e9543f5308d42e749
seronto <seronto8888@gmail.com>
2026-06-02 21:17:14 +0700

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const uuid = env.UUID || "ad80b5ad-c12e-4614-a95e-18e3fb77a064";
+    
+    if (url.pathname === '/sub') {
+      const host = url.hostname;
+      const vlessMain = `vless://${uuid}@${host}:443?encryption=none&security=tls&sni=${host}&type=ws&host=${host}&path=%2F#Seronto-VLESS`;
+      return new Response(btoa(vlessMain), {
+        headers: { 'content-type': 'text/plain; charset=utf-8' }
+      });
+    }
+    
+    return new Response("Skrip Aktif! Tambahkan /sub di ujung alamat untuk mengambil akun VLESS.", { status: 200 });
+  }
+};
